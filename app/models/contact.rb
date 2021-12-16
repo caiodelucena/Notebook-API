a9eef4644000a00941fda19c98efdadb4783f882
caiodelucena <caiodelucena6@gmail.com>
2021-12-16 09:19:40 -0300

    Set AMS adapter to json_api, add attibutes to contact and kind serializer

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,12 +6,12 @@ class Contact < ApplicationRecord
   accepts_nested_attributes_for :phones, allow_destroy: true
   accepts_nested_attributes_for :address, update_only: true
 
-  def as_json(option = {})
-    hash_toBr = super(
-        except: [:kind_id],
-        include: [{ kind: { only: :description } }, { phones: { only: :number } }, { address: { only: [:street, :city] } }] 
-      )
-    hash_toBr[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
-    hash_toBr
-  end
+  # def as_json(option = {})
+  #   hash_toBr = super(
+  #       except: [:kind_id],
+  #       include: [{ kind: { only: :description } }, { phones: { only: :number } }, { address: { only: [:street, :city] } }] 
+  #     )
+  #   hash_toBr[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+  #   hash_toBr
+  # end
 end
--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,3 +1,10 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :email, :birthdate
+
+  def attributes(*args)
+    hash_toBr = super(*args)
+    # pt-BR ---> hash_toBr[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+    hash_toBr[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+    hash_toBr
+  end
 end
