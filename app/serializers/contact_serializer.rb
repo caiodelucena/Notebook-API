46b4f191dea4c11d5d78a11fddc34d36b4a16697
caiodelucena <caiodelucena6@gmail.com>
2021-12-16 09:46:52 -0300

    Contact associations

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,6 +1,11 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
+  belongs_to :kind
+  has_many :phones
+  has_one :address
+
+
   def attributes(*args)
     hash_toBr = super(*args)
     # pt-BR ---> hash_toBr[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
