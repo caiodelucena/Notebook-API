4253b461d94be949664006944be1e8da68ba0063
caiodelucena <caiodelucena6@gmail.com>
2021-12-20 08:46:42 -0300

    Add relationship to address serializer

--- app/serializers/address_serializer.rb
+++ app/serializers/address_serializer.rb
@@ -1,3 +1,7 @@
 class AddressSerializer < ActiveModel::Serializer
   attributes :id, :street, :city
+
+  belongs_to :contact do 
+    link(:related) { (api_v1_contact_address_url(object.contact.id) unless object.contact.nil?) }
+  end
 end
