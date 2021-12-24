88ba052979acbf09dd54a78460dbd4f6113c4a55
caiodelucena <caiodelucena6@gmail.com>
2021-12-24 09:18:06 -0300

    Created the authenticate controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
       resources :contacts do
+        resource :address, except: [:index]
         resource :kind, only: [:show]
         resource :phones, only: [:show] 
         resource :phone, except: [:index, :show]
-        resource :address, except: [:index]
       end
+      resources :auths, only: [:create]
       resources :kinds
     end
   end
