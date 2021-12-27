569f7c0387302d6103f2bccb5303fa1d3c305515
caiodelucena <caiodelucena6@gmail.com>
2021-12-27 20:40:24 -0300

    learning about rack middleware

---
+++ lib/app_name.rb
@@ -0,0 +1,17 @@
+require 'rack'
+class AppName
+  def initialize(app, app_name)
+    @app = app
+    @app_name = app_name
+  end
+
+  def call(env)
+    if env["ORIGINAL_FULLPATH"] == "/"
+      ['200', {'Content-Type' => 'text/html'}, ["Notebook-API Index"]]
+    else
+      status, headers, response = @app.call(env)
+      headers.merge!({'X-App-Name' => "#{@app_name}"})
+      [status, headers, [response.body]]
+    end
+  end
+end
